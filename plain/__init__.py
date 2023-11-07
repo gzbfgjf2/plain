985fc705663edc559d6939ba76643088b796d79c
gzbfgjf2 <142939562+gzbfgjf2@users.noreply.github.com>
2023-11-07 22:47:12 +0000

    Add first commit

---
+++ plain/__init__.py
@@ -0,0 +1,2 @@
+def test_plain():
+    print(1)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='plain',
+    packages=find_packages(include=['plain']),
+    version='0.0.1',
+    description='Plain machine learning building blocks for research purpose',
+    author='gzbfgjf2',
+)
