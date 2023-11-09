bf6d935990289689539707775673ef3d84beddc2
gzbfgjf2 <142939562+gzbfgjf2@users.noreply.github.com>
2023-11-09 08:19:48 +0000

    Improve decoder-encoder attention

--- plain/__init__.py
+++ plain/__init__.py
@@ -14,27 +14,30 @@ class AttendToEncoding(nn.Module):
         self.n_embd = config.n_embd
         self.dropout = config.dropout
 
-        self.q = nn.Linear(self.n_embd, self.n_embd, bias=config.bias)
-        self.k = nn.Linear(self.n_embd, self.n_embd, bias=config.bias)
-        self.v = nn.Linear(self.n_embd, self.n_embd, bias=config.bias)
-        self.projection = nn.Linear(
-            config.n_embd, config.n_embd, bias=config.bias
-        )
+        linear = lambda: nn.Linear(self.n_embd, self.n_embd, bias=config.bias)
+
+        self.q = linear()
+        self.k = linear()
+        self.v = linear()
+        self.projection = linear()
         self.attention_dropout = nn.Dropout(config.dropout)
         self.residue_dropout = nn.Dropout(config.dropout)
 
     def forward(self, decoder_hs, encoder_hs):
-        (db, dt, dc) = decoder_hs.size()
-        (eb, et, ec) = encoder_hs.size()
-        assert db == eb
+        (b, s, d) = decoder_hs.size()
         q = self.q(decoder_hs)
         k = self.k(encoder_hs)
         v = self.v(encoder_hs)
 
-        q = q.view(db, dt, self.n_head, dc // self.n_head).transpose(1, 2)
-        k = k.view(eb, et, self.n_head, ec // self.n_head).transpose(1, 2)
-        v = v.view(eb, et, self.n_head, ec // self.n_head).transpose(1, 2)
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        heads_view = lambda tensor: tensor.view(
+            b, s, self.n_head, d // self.n_head
+        ).transpose(1, 2)
+
+        q = heads_view(q)
+        k = heads_view(k)
+        v = heads_view(v)
+
+        # (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         # manual implementation of attention
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
         att = F.softmax(att, dim=-1)
@@ -42,7 +45,7 @@ class AttendToEncoding(nn.Module):
         # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = att @ v
         y = (
-            y.transpose(1, 2).contiguous().view(db, dt, dc)
+            y.transpose(1, 2).contiguous().view(b, s, d)
         )  # re-assemble all head outputs side by side
 
         # output projection
