13e98d872a3963be59825bc0b1dab54788f5ca76
gzbfgjf2 <142939562+gzbfgjf2@users.noreply.github.com>
2023-11-18 00:44:41 +0000

    Generalise attention Layer

--- plain/__init__.py
+++ plain/__init__.py
@@ -6,48 +6,68 @@ import math
 # reference: https://github.com/karpathy/nanoGPT
 
 
-class AttendToEncoding(nn.Module):
+def causal_mask(sequence_length):
+    mask = torch.ones(sequence_length, sequence_length)
+    mask = mask.view(1, 1, sequence_length, sequence_length)
+    mask = torch.tril(mask)
+    return mask
+
+
+MASK = {"causal": causal_mask}
+
+
+def scaled_dot_product_attention(q, k, v, dropout, mask):
+    att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+    if mask:
+        (b, s, d) = k.size()
+        att = att.masked_fill(mask[:, :, :s, :s] == 0, float("-inf"))
+    att = F.softmax(att, dim=-1)
+    att = dropout(att)
+    y = att @ v
+    return y
+
+
+class AttentionLayer(nn.Module):
     def __init__(self, config):
         super().__init__()
-        assert config.n_embd % config.n_head == 0
+        assert config.n_d % config.n_head == 0
+        self.n_d = config.n_d
         self.n_head = config.n_head
-        self.n_embd = config.n_embd
+        self.n_head_d = self.n_d // self.n_head
         self.dropout = config.dropout
+        self.bias = config.bias
+        self.sequence_length = config.sequence_length
+        self.mask_name = config.mask_name
 
-        linear = lambda: nn.Linear(self.n_embd, self.n_embd, bias=config.bias)
-
+        linear = lambda: nn.Linear(self.n_d, self.n_d, bias=self.bias)
         self.q = linear()
         self.k = linear()
         self.v = linear()
         self.projection = linear()
-        self.attention_dropout = nn.Dropout(config.dropout)
-        self.residue_dropout = nn.Dropout(config.dropout)
+        self.attention_dropout = nn.Dropout(self.dropout)
+        self.residue_dropout = nn.Dropout(self.dropout)
+        if self.mask_name:
+            self.register_buffer(
+                "mask", MASK[self.mask_name](self.sequence_length)
+            )
+        else:
+            self.mask = None
 
     def forward(self, decoder_hs, encoder_hs):
+        # batch, sequence, dimension
         (b, s, d) = decoder_hs.size()
         q = self.q(decoder_hs)
         k = self.k(encoder_hs)
         v = self.v(encoder_hs)
 
         heads_view = lambda tensor: tensor.view(
-            b, s, self.n_head, d // self.n_head
+            b, s, self.n_head, self.n_head_d
         ).transpose(1, 2)
 
         q = heads_view(q)
         k = heads_view(k)
         v = heads_view(v)
-
-        # (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        # manual implementation of attention
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = F.softmax(att, dim=-1)
-        att = self.attention_dropout(att)
-        # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = att @ v
-        y = (
-            y.transpose(1, 2).contiguous().view(b, s, d)
-        )  # re-assemble all head outputs side by side
-
-        # output projection
+        y = scaled_dot_product_attention(q, k, v, self.dropout, self.mask)
+        y = y.transpose(1, 2).contiguous().view(b, s, d)
         y = self.residue_dropout(self.projection(y))
         return y
